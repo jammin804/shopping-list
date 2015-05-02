dcb539794749642aebf86149832a64552023a9a8
jammin804 <jammin804@gmail.com>
2015-05-02 10:49:41 -0400

    Finally fixed the the new element being found by using jQuery .on method

--- js/app.js
+++ js/app.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
   /*This function adds the information that is the text field as a list item. This event is triggered when the user is in the text field and press the Enter/Return key on their keyboard*/
   $('.collaspe-top-prioity-item').keydown(function(e) {
     if (e.which == 13) {
-      $(this).next().prepend("<li>" + $(this).val() + "<div class='check_minus'><i class='fa fa-check'></i><i class='fa fa-minus'></i></div></li>");
+      $(this).next().prepend("<li class='item'>" + $(this).val() + "<div class='check_minus'><i class='fa fa-check'></i><i class='fa fa-minus'></i></div></li>");
       $(this).val('');
     }
   });
   $('.collaspe-prioity-item').keydown(function(e) {
     if (e.which == 13) {
-      $(this).next().prepend("<li>" + $(this).val() + "<div class='check_minus'><i class='fa fa-check'></i><i class='fa fa-minus'></i></div></li>");
+      $(this).next().prepend("<li class='item'>" + $(this).val() + "<div class='check_minus'><i class='fa fa-check'></i><i class='fa fa-minus'></i></div></li>");
       $(this).val('');
     }
   });
   $('.collaspe-wants-item').keydown(function(e) {
     if (e.which == 13) {
-      $(this).next().prepend("<li>" + $(this).val() + "<div class='check_minus'><i class='fa fa-check'></i><i class='fa fa-minus'></i></div></li>");
+      $(this).next().prepend("<li class='item'>" + $(this).val() + "<div class='check_minus'><i class='fa fa-check'></i><i class='fa fa-minus'></i></div></li>");
       $(this).val('');
     }
   });//<-- End of this interaction
@@ -29,9 +29,14 @@ $(document).ready(function() {
     $('.collaspe-wants-item').slideToggle();
   });//<-- End of this interaction
   //Crosses through the text when the checkmark is checked
-  $('.fa-check').on('click', function(){
+  $(document).on('click', '.fa-check', function(){
     // $('.top-prioity-item-1').toggleClass('complete');
-    $(this).parent().closest('li').css('textDecoration', 'line-through');
+    $(this).parent().closest('li.item').css('textDecoration', 'line-through');
+    alert('Here!');
   });
   //Removes the text from the list when the minus button is clicked
+  $('.fa-minus').live('click', function(){
+    // $('.top-prioity-item-1').toggleClass('complete');
+    $(this).parent().closest('li.item').remove();
+  });
 });
